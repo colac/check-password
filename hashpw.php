e8f329d0c8e1cdf2fd7635b4592e5d01fb31f337
colac <hugof_lacerda@hotmail.com>
2018-04-18 12:11:24 +0100

    Add files via upload

---
+++ hashpw.php
@@ -0,0 +1,21 @@
+<?php
+echo strtoupper("Hello WORLD!");
+if (isset($_POST['passwordToHash'])){
+//phpinfo();
+//echo $_POST['passwordToHash'];
+
+$password = $_POST['passwordToHash'];
+$hashedPass = sha1($password);
+$hashedUpper = mb_strtoupper($hashedPass);
+//echo $hashedUpper;
+//echo $hashedPass;
+//print_r($_POST);
+header("Location: https://api.pwnedpasswords.com/pwnedpassword/$hashedUpper");
+}
+else {
+	header("Location:./index.html");
+	exit();
+	//print_r($_POST);		
+}
+
+?>
